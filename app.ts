62a5a8798b64d0fdcf2993759e8742e08d1f95c4
Jennas-Lee <leezeunjun@gmail.com>
2022-02-12 14:08:57 +0900

    Add app.ts

---
+++ app.ts
@@ -0,0 +1,31 @@
+import express, { Request, Response, NextFunction } from 'express';
+
+interface ErrorWithStatus extends Error{
+  status: number;
+}
+
+const NODE_ENV: string = process.env.NODE_ENV || 'development';
+const PORT: number = parseInt(process.env.EXPRESS_PORT || '3001');
+
+const app: express.Application = express();
+
+app.set('port', PORT);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  let err = new Error() as ErrorWithStatus;
+  err.status = 404;
+  next(err);
+});
+
+app.use((err: ErrorWithStatus, req: Request, res: Response, next: NextFunction) => {
+  res.locals.error = NODE_ENV === 'development' ? err : {};
+  res.status(err.status || 500).send();
+  next();
+});
+
+app.listen(app.get('port'), () => {
+  console.log('Server is listening on port :', app.get('port'));
+});
